45b6a673567abfa9de933898411a9aa9cee22b83
ddorn <diego.dorn@free.fr>
2020-05-25 04:49:31 +0200

    :sparkles: coloration of the prompt

--- splendor/tui/client.py
+++ splendor/tui/client.py
@@ -1,5 +1,10 @@
+import re
+
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import Completer, Completion, NestedCompleter
+from prompt_toolkit.contrib.regular_languages import compile
+from prompt_toolkit.contrib.regular_languages.lexer import GrammarLexer
+from prompt_toolkit.lexers import PygmentsLexer
 from prompt_toolkit.output import ColorDepth
 from prompt_toolkit.validation import ValidationError, Validator
 
@@ -7,6 +12,62 @@ from data import *
 from game import *
 from game.errors import ActionParseError
 from splendor.runner import BaseClient
+
+
+from prompt_toolkit.lexers import Lexer
+from prompt_toolkit.shortcuts import prompt
+from prompt_toolkit.styles.named_colors import NAMED_COLORS
+
+
+class ActionLexer(Lexer):
+    def lex_document(self, document):
+        colors = list(sorted(NAMED_COLORS, key=NAMED_COLORS.get))
+
+        # Colors can be named
+        rev = {color: i for i, color in COINS_TO_NAMES.items()}
+        # Or digits
+        rev.update({str(i): i for i in range(YELLOW + 1)})
+
+        def split_words(s: str):
+            cur = ""
+            for l in s:
+                if l.isspace() and cur.strip():
+                    yield cur
+                    cur = ""
+                elif not l.isspace() and not cur.strip():
+                    yield cur
+                    cur = ""
+                cur += l
+            if cur:
+                yield cur
+
+        def get_line(lineno):
+            text = document.lines[lineno]
+            lex = []
+
+            words = list(split_words(text))
+
+            for w in words[1:]:
+                if w.upper() in ["TAKE", "BUY", "RESERVE"]:
+                    lex.append(("#ffffff", w))
+                elif w.lower() in rev:
+                    color = {
+                        RED: "#FF0000",
+                        GREEN: "#00FF00",
+                        BLUE: "#0000FF",
+                        BLACK: "#C06060",
+                        WHITE: "#FFFFFF",
+                        YELLOW: "#FFFF00",
+                    }.get(rev[w.lower()])
+                    lex.append((color, w))
+                elif w.isnumeric():
+                    lex.append(("fg:pink", w))
+                else:
+                    lex.append(("", w))
+
+            return lex
+
+        return get_line
 
 
 class ColorCompleter(Completer):
@@ -49,6 +110,7 @@ class TuiClient(BaseClient):
             completer=completer,
             validator=ActionValidator(),
             color_depth=ColorDepth.TRUE_COLOR,
+            lexer=ActionLexer(),
         )
 
         action = Action.from_str(text)
